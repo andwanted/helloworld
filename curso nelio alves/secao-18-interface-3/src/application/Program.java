584e27b615d8dda22748634c378ffb0c628e3a85
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-05-29 16:58:03 -0300

    exercicio sobre problema diamente, interface + classe abs

--- curso nelio alves/secao-18-interface-3/src/application/Program.java
+++ curso nelio alves/secao-18-interface-3/src/application/Program.java
@@ -1,21 +1,25 @@
 package application;
 
-import entities.Scanner;
-import entities.Printer;
+import entities.ConcretScanner;
+import entities.ComboDevice;
+import entities.ConcretPrinter;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Printer p = new Printer("1080");
+
+		ConcretPrinter p = new ConcretPrinter("1080");
 		p.processDoc("My Letter");
 		p.print("My Letter");
-		
-		Scanner s = new Scanner("2003");
+		System.out.println("#########################");
+		ConcretScanner s = new ConcretScanner("2003");
 		s.processDoc("My Email");
 		System.out.println("Scan result: " + s.scan());
-		
+		System.out.println("#########################");
+		ComboDevice c = new ComboDevice("SERIECOMBO1312331");
+		c.processDoc("My dissertation");
+		c.print("My dissertation");
+		System.out.println("Resultado do scan Combo .." + c.scan());
 	}
 
-	
 }
