5ed9997b21edb0816103a51847c148b8d31f5344
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-06-14 13:14:08 -0300

    limpando arquivos que não são importantes, organizando pastas, para curso, exercicio curso, e exercicios propios

--- exercicios curso java nelio alves/ws-exercicios/secao-18-exercicio-propio/src/services/ServicoFinanciamento.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-public interface ServicoFinanciamento {
-
-	double AvaliarVeiculo(int ano, double valorVeiculo);
-	boolean AvaliarComprador(int registro,double salario,double fgts);
-	
-}
