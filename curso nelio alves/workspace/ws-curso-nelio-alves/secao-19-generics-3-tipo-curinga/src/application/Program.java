6052c27a284ea8440596060ec7edb2a301b7c6ee
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-06-23 16:26:12 -0300

    adicionado exemplo de coleção tipo curinga

---
+++ curso nelio alves/workspace/ws-curso-nelio-alves/secao-19-generics-3-tipo-curinga/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		List<Double> myInts = Arrays.asList(5.1655454, 2.1, 10.2);
+		printList(myInts);
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+}
