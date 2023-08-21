583804c65ba6a61ff12c6514b91f4023ab40f13e
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-08-21 19:10:23 -0300

    Principio get/put - contravariância

--- exemplos de uso/PrincipioGetPut/src/application/Program.java
+++ exemplos de uso/PrincipioGetPut/src/application/Program.java
@@ -8,15 +8,14 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
-		List<? extends Number> list = intList;
-		Number x = list.get(0);
-		//list.add(20); // erro de compilacao
-		
-		
-		//get - OK put - ERROR
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+		//Number x = myNums.get(0); // erro de compilacao
+		//get - ERROR put - OK
 		
 	}
 
