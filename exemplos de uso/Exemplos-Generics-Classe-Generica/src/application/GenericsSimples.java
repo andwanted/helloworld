f9d87b98310058b317a5a151d2eb626a2de9b51d
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-08-22 14:26:20 -0300

    exemplo de uso generics 2 - problema metodo generico

--- exemplos de uso/Exemplos-Generics-Classe-Generica/src/application/GenericsSimples.java
+++ exemplos de uso/Exemplos-Generics-Classe-Generica/src/application/GenericsSimples.java
@@ -9,15 +9,46 @@ import services.ApresentaClasse;
 public class GenericsSimples {
 
 	public static void main(String[] args) {
+
+		List<Integer> quantity = new ArrayList<>(List.of(1,2,3,4,5));
 		
-		ApresentaClasse apresentaClasse = new ApresentaClasse();
+		System.out.println("Resultado da soma da lista é: " +  calcular(quantity));
 		
-		apresentaClasse.addValue("Valor em String 1");
-		apresentaClasse.addValue(1);
+		List<Double> quantityDouble = new ArrayList<>(List.of(10.0,20.0,30.0,40.0,50.0));
 		
-		apresentaClasse.apresentar();
+		//Sem o uso de um método generico, meu método imprime apenas um tipo, precisando criar outro serviço para imprimir tipos double
+		System.out.println("Resultado da soma da lista é: " +  calcularDouble(quantityDouble));
+		
 	}
 
+	public static Integer calcular(List<Integer> list) {
 
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty");
+		}
+
+		Integer sum = list.get(0);
+
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+
+		return sum;
+	}
+	
+	public static Double calcularDouble(List<Double> list) {
+
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty");
+		}
+
+		Double sum = list.get(0);
+
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+
+		return sum;
+	}
 
 }
