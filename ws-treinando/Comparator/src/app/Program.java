165a13171785d1749d16f3f84ade1f0402ae1481
Anderson leandro <andersonleandrooo@hotmail.com>
2024-09-19 22:17:41 -0300

    testando comparator, forma 3 expressao lambda com chaves

--- ws-treinando/Comparator/src/app/Program.java
+++ ws-treinando/Comparator/src/app/Program.java
@@ -17,14 +17,9 @@ public class Program {
 		list.add(new Product("Bola", 30.00));
 		list.add(new Product("Tesoura", 10.00));
 
-		// Comparator objeto de classe anônima
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product p1, Product p2) {
-				// TODO Auto-generated method stub
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		// Comparator com expressão lambda com chaves
+		Comparator<Product> comp =(p1,p2) ->{
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
 		
 		list.sort(comp);
